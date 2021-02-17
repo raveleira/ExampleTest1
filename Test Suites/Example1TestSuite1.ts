339478587395b0b3db383ffc55204870b59098b9
Mune <Mune@HOME-PC>
2021-02-17 11:22:10 -0500

    Adding Example1TestSuite1

---
+++ Test Suites/Example1TestSuite1.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Example1TestSuite1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>ca4859c0-3819-433a-8a83-e67d1fc1db0a</testSuiteGuid>
+</TestSuiteEntity>
